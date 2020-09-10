d8d3894e8e73d8004af2c3c9e882a9bcd4565c24
royeradames <royeraadames@gmail.com>
2020-09-10 18:13:54 -0400

    added ingredients data to see

--- data/seeds/01-recipes.js
+++ data/seeds/01-recipes.js
@@ -3,7 +3,6 @@ exports.seed = function (knex, Promise) {
   return knex('recipes').insert([
     {
       recipe_name: "Scramble eggs",
-      steps: '1. get eggs. 2. scramble the eggs'
     },
   ]);
 };
