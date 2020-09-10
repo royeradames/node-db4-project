c7c1479c3cd1c706ddc00d61834ef1d141b92701
royeradames <royeraadames@gmail.com>
2020-09-10 19:47:49 -0400

    added helper functions to recipes module

---
+++ recipes/recipes-module.js
@@ -0,0 +1,17 @@
+module.imports = {
+    getRecipes,
+    getShoppingList,
+    getInstructions,
+}
+//should return a list of all recipes in the database.
+function getRecipes () {
+    
+}
+// should return a list of all ingredients and quantities for a given recipe
+function getShoppingList (recipe_id) {
+    
+}
+// should return a list of step by step instructions for preparing a recipe
+function getInstructions (recipe_id) {
+    
+}
