7608ab1af6e50c733cb8499b65b66f4124587891
royeradames <royeraadames@gmail.com>
2020-09-10 19:56:43 -0400

    completed sql for getRecipes, and getShoppingList helper functions

--- recipes/recipes-module.js
+++ recipes/recipes-module.js
@@ -6,14 +6,27 @@ module.imports = {
     getInstructions,
 }
 //should return a list of all recipes in the database.
-function getRecipes () {
-    
+function getRecipes() {
+    /*
+        select *
+        from recipes
+        ;
+    */
+
 }
 // should return a list of all ingredients and quantities for a given recipe
-function getShoppingList (recipe_id) {
-    
+function getShoppingList(recipe_id) {
+    /*
+        select i.ingredient_name, ri.quantity
+        from ingredients as i
+        join recipes_ingredients as ri
+            on i.id = ri.ingredient_id
+        where recipe_id = 1
+        ;
+    */
+
 }
 // should return a list of step by step instructions for preparing a recipe
-function getInstructions (recipe_id) {
-    
+function getInstructions(recipe_id) {
+
 }
