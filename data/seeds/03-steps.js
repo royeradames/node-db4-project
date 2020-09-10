4bdb09fee666c6ba570e9d1c0d3f78041d1b204c
royeradames <royeraadames@gmail.com>
2020-09-10 18:19:06 -0400

    added seeds data for ingredients and steps

--- data/seeds/03-steps.js
+++ data/seeds/03-steps.js
@@ -3,17 +3,7 @@
 exports.seed = function(knex, Promise) {
   return knex('animals').insert([
     // species_id must match valid species
-    { animal_name: "Snuffles", species_id: 1 },
-    { animal_name: "Cornelius", species_id: 2 },
-    { animal_name: "Athena", species_id: 3 },
-    { animal_name: "Ares", species_id: 3 },
-    { animal_name: "Snelby", species_id: 4 },
-    { animal_name: "Gwendolyn", species_id: 5 },
-    { animal_name: "Archebald", species_id: 6 },
-    { animal_name: "Polonius", species_id: 1 },
-    { animal_name: "Augusta", species_id: 4 },
-    { animal_name: "Stephen", species_id: 7 },
-    { animal_name: "Rocky", species_id: 8 },
-    // { animal_name: "Bellatrix", species_id: 19 }
+    { step: "In a medium bowl, use a fork to whisk together eggs and yogurt. ", recipe_id: 1, order: 1},
+    { step: "In a medium nonstick pan over medium-low heat, melt butter. When butter is completely melted, add eggs. When eggs start to set, use a rubber spatula to gently move the cooked egg around in the pan. When the eggs are almost cooked to your liking, fold in chives and plate. Season with salt and pepper.  ", recipe_id: 1, order: 2 },
   ]);
 };
