ed86621a79ec414d30a3bbf6505585d210d636c4
royeradames <royeraadames@gmail.com>
2020-09-11 10:17:34 -0400

    finish GET /api/ingredients/:id/recipes: all recipes in the system that utilize a single ingredient

--- ingredients/ingredients-module.js
+++ ingredients/ingredients-module.js
@@ -1,6 +1,21 @@
 const db = require('../data/db-config.js');
 
 module.exports = {
-    
+    getIngredientForRecipes
 }
 
+// all recipes in the system that utilize a single ingredient
+function getIngredientForRecipes(ingredient_id) {
+    /*
+        select r.recipe_name
+        from recipes_ingredients as ri
+        join recipes as r
+            on r.id = ri.recipe_id
+        where ri.ingredient_id = 1
+        ;
+    */
+    return db('recipes_ingredients as ri')
+        .where('ri.ingredient_id', ingredient_id)
+        .join('recipes as r', 'r.id', 'ri.recipe_id')
+        .select('r.recipe_name')
+}
--- ingredients/ingredients-router.js
+++ ingredients/ingredients-router.js
@@ -1,11 +1,10 @@
 const express = require('express')
-const Recipes = require('./recipes-module')
+const Recipes = require('./ingredients-module')
 const router = express.Router()
 
 router.get('/:id/recipes', (req, res) => {
-    Recipes.getRecipes()
+    Recipes.getIngredientForRecipes(req.params.id)
         .then(recipes => {
-
             if (recipes.length) {
                 res.status(200).json(recipes)
             } else {
