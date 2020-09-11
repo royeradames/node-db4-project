b09eef686157db2db4bae19b9bbda651b32fcef1
royeradames <royeraadames@gmail.com>
2020-09-10 21:47:23 -0400

    building recipes router

---
+++ recipes/recipes-router.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const Recipes = require('./recipes-module')
+const router = express.Router()
+
+router.get('/', (req,res) =>{
+    Recipes.getRecipes()
+        .then(recipes => {
+            
+            if (recipes.length) {
+                res.status(200).json(recipes)
+            } else {
+                res.status(400).json({recipes: 'There is no recipes.'})
+            }
+        })
+        .catch(error => {
+            res.status(500).json({error: error.message})
+
+        })
+})
+
+module.exports = router
