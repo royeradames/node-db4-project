c41a8385b2bba5a9f678e6e50a57cb82d460ae3e
royeradames <royeraadames@gmail.com>
2020-09-11 10:03:21 -0400

    seet up ingredientsRouter on server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 const recipesRouter = require('./recipes/recipes-router')
+const ingredientsRouter = require('./ingredients/ingredients-router')
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use('/api/recipes', recipesRouter)
+server.use('/api/ingredients', ingredientsRouter)
 
 server.get('/', (req, res) => {
   
